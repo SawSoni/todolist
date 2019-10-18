442b524b12c03a825f4b40f19e742cd3fc12e731
soni saw <soni.saw@kuliza.com>
2019-10-18 16:12:55 +0530

    added routing file

---
+++ src/Routers.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams,
+    Redirect
+  } from "react-router-dom";
+
+import Login from './Containers/Login/Login';
+import About from './Containers/About/About';
+
+
+
+
+class Routers extends Component {
+    render(){
+        return(
+            <Switch>
+                <Route path="/"  exact strict component={Login}></Route>
+                {/* <Route path="/login" component={Login}></Route> */}
+                <Route path="/about" component={About}></Route>
+                <Route
+                 render={function () {
+                     return <h1>Not Found</h1>;
+                 }}
+                 />
+            </Switch>
+        )
+    }
+}
+
+export default Routers
