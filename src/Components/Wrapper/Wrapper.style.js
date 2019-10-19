f597f09dd55c2b67eb3f8ae8bf755bbc7f348325
soni saw <soni.saw@kuliza.com>
2019-10-19 16:40:46 +0530

    created a wrapper component

---
+++ src/Components/Wrapper/Wrapper.style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+
+export const StyledWrapper = styled.div`
+    width: ${props => props.width};
+    margin: ${props => props.margin};
+    /* border: 2px solid red; */
+
+`
