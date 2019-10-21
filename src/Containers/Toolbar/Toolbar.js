227256f965a14844751dcf58b651936b3ce94a43
soni saw <soni.saw@kuliza.com>
2019-10-21 17:33:54 +0530

    removed console

--- src/Containers/Toolbar/Toolbar.js
+++ src/Containers/Toolbar/Toolbar.js
@@ -24,7 +24,6 @@ class Toolbar extends Component {
     }
 
     render() { 
-        console.log("this is toolbar component-----------------",this.props);
         return (
             <Header>
                 <Nav>
