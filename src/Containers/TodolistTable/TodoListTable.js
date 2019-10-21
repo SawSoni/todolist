d1f3b4cbcea7093cf76f959a40cc255f67b2555f
soni saw <soni.saw@kuliza.com>
2019-10-21 17:33:29 +0530

    created todotable to show list

---
+++ src/Containers/TodolistTable/TodoListTable.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Wrapper from '../../Components/Wrapper/Wrapper'
+import { HeaderRow, Col, Row, RowContainer, FooterRow } from './TodoListTable.styled';
+import Button from '../../Components/Button/Button';
+
+class ToDoListTable extends Component {
+    state = {
+        tableHeader : [ 'Date', 'Todo', 'Status', `Button` ],
+    }
+
+    tableHeader = () => {
+        const { tableHeader } = this.state;
+        const columns = tableHeader.map((data,index) => <Col key={index}>{ data }</Col>)
+        return(
+            <HeaderRow>
+                    {columns}
+            </HeaderRow>
+        )
+    }
+
+    createTodoList = () => {
+        const { list } = this.props;
+        const todoList = list.map((item, index) => <Row key={index}>
+            <Col>{item.date}</Col>
+            <Col>{item.task}</Col>
+            <Col>{item.status}</Col>
+            <Col><Button text="Done" color="#ABC" onclick={() => item.updateStatus(index)}></Button></Col>
+        </Row>)
+        return(
+            <RowContainer>
+                {todoList}
+            </RowContainer>
+            
+        )
+    }
+
+    render(){
+        return(
+            <Wrapper>
+                {this.tableHeader()}
+                {this.createTodoList()}
+                <FooterRow></FooterRow>
+            </Wrapper>
+        )
+    }
+}
+
+export default ToDoListTable
+
