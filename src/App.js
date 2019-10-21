2c24d0e3355d1c4fb35f5942975fa558f466bbaa
soni saw <soni.saw@kuliza.com>
2019-10-21 15:14:17 +0530

    removed class name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
-import Routers from  './Routers'
+import Routers from  './Routers';
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div>
         <Routers>
         </Routers>
       </div>
