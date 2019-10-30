85a8d7aa50e8b39180c8d6bac8e3b39aa799b908
soni saw <soni.saw@kuliza.com>
2019-10-30 12:38:06 +0530

    In store i have combined todo reducer and user reducer

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,6 +1,10 @@
 
-import { createStore } from "redux";
-import rootReducer from "../reducers/index";
-const store = createStore(rootReducer);
+import { createStore, combineReducers } from "redux";
+import todoReducer from "../reducers/index";
+import userReducer from "../../Containers/User/reducers"
+
+// var composeEnhancers = window._REDUX_DEVTOOL_EXTENTION_COMPOSE_ || compose
+const reducer = combineReducers({todoReducer: todoReducer, userReducer: userReducer})
+const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
