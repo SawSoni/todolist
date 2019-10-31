cef648531acb6dbbe29ece873e3189e2094cb757
soni saw <soni.saw@kuliza.com>
2019-10-31 17:24:36 +0530

    enable redux dev tool and saga is running now

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,10 +1,16 @@
 
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import todoReducer from "../reducers/index";
-import userReducer from "../../Containers/User/reducers"
+import userReducer from "../../Containers/User/reducers";
+import createSagaMiddleware from 'redux-saga';
+import rootSaga  from '../../Containers/User/saga';
 
-// var composeEnhancers = window._REDUX_DEVTOOL_EXTENTION_COMPOSE_ || compose
+const sagaMiddleware = createSagaMiddleware();
+const composeEnhancers2 = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+      : compose;
 const reducer = combineReducers({todoReducer: todoReducer, userReducer: userReducer})
-const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(reducer,composeEnhancers2(applyMiddleware(sagaMiddleware)))
+sagaMiddleware.run(rootSaga);
 
 export default store;
