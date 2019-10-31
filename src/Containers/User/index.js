e878010e36cd74651735f1fad1fd816caec47a3c
soni saw <soni.saw@kuliza.com>
2019-10-31 17:18:39 +0530

    In user index adde header component

--- src/Containers/User/index.js
+++ src/Containers/User/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import UserForm from './UserForm';
 import Wrapper from '../../Components/Wrapper/Wrapper';
+import Header from '../../Containers/Header/Header'
 
 class User extends Component {
     render(){
         return(
             <Wrapper>
+                <Header></Header>
                 <UserForm></UserForm>
             </Wrapper>
         )
