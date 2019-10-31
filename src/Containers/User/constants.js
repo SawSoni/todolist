0c93a51c5455647d5634894cb73c1c2339e8318f
soni saw <soni.saw@kuliza.com>
2019-10-31 17:18:00 +0530

    created constant to use in reducer and store file

--- src/Containers/User/constants.js
+++ src/Containers/User/constants.js
@@ -1,1 +1,5 @@
 export const ADD_USER = 'add_user';  
+export const GET_USERS = 'get_users';
+export const GET_USERS_ASYNC = 'get_user_async';
+export const ADD_USER_DEMO = 'add_user_demo';
+export const ADD_USER_DEMO_ASYNC = 'add_user_demo_async';
