a5d239178cfb7f2dc19ad5d1ce310ce58da32696
soni saw <soni.saw@kuliza.com>
2019-10-31 17:17:13 +0530

    creted action creater function for user to add user data in store and connect with saga

--- src/Containers/User/actions.js
+++ src/Containers/User/actions.js
@@ -1,6 +1,23 @@
-import { ADD_USER } from "./constants"
+import { ADD_USER, GET_USERS, GET_USERS_ASYNC, ADD_USER_DEMO, ADD_USER_DEMO_ASYNC  } from "./constants"
 
 
 export function addUser(payload) {
     return { type: ADD_USER, payload }
+}
+
+export function getUsers() {
+    return { type: GET_USERS}
+}
+
+export function getUserAsyc(payload){
+    return { type: GET_USERS_ASYNC, payload}
+}
+
+export function addUserDemo(payload) {
+    return { type: ADD_USER_DEMO, payload}
+}
+
+export function addUserDemoAsync(payload){
+
+    return{ type: ADD_USER_DEMO_ASYNC, payload}
 }
