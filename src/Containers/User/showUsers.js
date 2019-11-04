bfddd1fb025732d9b0f0f1d2407648033a21a811
soni saw <soni.saw@kuliza.com>
2019-11-04 17:17:11 +0530

    added a unique key for each elememt

--- src/Containers/User/showUsers.js
+++ src/Containers/User/showUsers.js
@@ -5,7 +5,7 @@ const ShowUsername = (props) => {
     console.log("this is show user here", props)
     return(
         <div>
-            {props.users.map((item) => <p>{item.name}</p>)}
+            {props.users.map((item,index) => <p key={index}>{item.name}</p>)}
         </div>
     )
 }
